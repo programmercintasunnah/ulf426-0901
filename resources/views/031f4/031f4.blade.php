5af4da738c1c406ecc4a583bbab4e121a1c8b760
programmercintasunnah <muhammadzakie22@gmail.com>
2021-09-26 00:05:14 +0700

    yeay 20 thn

---
+++ resources/views/031f4/031f4.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Webpage Title -->
+        <title>20 tahun ulfa</title>
+
+        <!-- Styles -->
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap"
+            rel="stylesheet">
+        <link href="{{'css'}}/bootstrap.min.css" rel="stylesheet">
+        <link href="{{'css'}}/fontawesome-all.min.css" rel="stylesheet">
+        <link href="{{'css'}}/swiper.css" rel="stylesheet">
+        <link href="{{'css'}}/styles.css" rel="stylesheet">
+
+        <!-- Favicon  -->
+        <link rel="icon" href="{{'images/'}}ulf.jpg">
+    </head>
+
+    <body>
+
+        <!-- Navigation -->
+        <nav id="navbar" class="navbar navbar-expand-lg fixed-top navbar-light" aria-label="Main navigation">
+            <div class="container">
+
+                <!-- Image Logo -->
+                <a class="navbar-brand logo-image" href="/"><img src="{{'images/'}}ulf.jpg" alt="alternative"> ULFA 20
+                    TAHUN</a>
+
+                <!-- Text Logo - Use this if you don't have a graphic logo -->
+                <!-- <a class="navbar-brand logo-text" href="/">Yavin</a> -->
+            </div> <!-- end of container -->
+        </nav> <!-- end of navbar -->
+        <!-- end of navigation -->
+
+
+        <!-- Header -->
+        <header id="header" class="header">
+            <img class="decoration-star" src="{{'images/'}}decoration-star.svg" alt="alternative">
+            <img class="decoration-star-2" src="{{'images/'}}decoration-star.svg" alt="alternative">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7 col-xl-5">
+                        <div class="text-container">
+                            <h1 class="h1-large">ULFA DAMAYANTI</h1>
+                            <p class="p-large">semoga di umur 20 tahun ini, kamu semakin berilmu dan semakin bertaqwa
+                                kepada allah. semoga umur mu diberkahi oleh allah.</p>
+                            <a class="btn-solid-lg" href="#introduction">26 September 2021</a>
+                            <a class="btn-outline-lg" href="#contact">20 Tahun</a>
+                        </div> <!-- end of text-container -->
+                    </div> <!-- end of col -->
+                    <div class="col-lg-5 col-xl-7">
+                        <div class="image-container">
+                            <img class="img-fluid" src="{{'images/'}}ulf.jpg" alt="alternative">
+                        </div> <!-- end of image-container -->
+                    </div> <!-- end of col -->
+                </div> <!-- end of row -->
+            </div> <!-- end of container -->
+        </header> <!-- end of header -->
+        <!-- end of header -->
+
+        <!-- Scripts -->
+        <script src="{{'js/'}}bootstrap.min.js"></script> <!-- Bootstrap framework -->
+        <script src="{{'js/'}}swiper.min.js"></script> <!-- Swiper for image and text sliders -->
+        <script src="{{'js/'}}purecounter.min.js"></script> <!-- Purecounter counter for statistics numbers -->
+        <script src="{{'js/'}}scripts.js"></script> <!-- Custom scripts -->
+    </body>
+
+</html>
